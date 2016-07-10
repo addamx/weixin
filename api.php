0139829028a819a367d90e8ef998c7dfa7f7283e
addams <haoranclover@gmail.com>
2016-07-10 15:31:58 +0800

    mod app

--- api.php
+++ api.php
@@ -55,20 +55,7 @@ class wechatCallbackapiTest
             <Content><![CDATA[%s]]></Content>
             </xml>
 EOF;
-            $musicTpl = <<<'EOF'
-            <xml>
-            <ToUserName><![CDATA[%s]]></ToUserName>
-            <FromUserName><![CDATA[%s]]></FromUserName>
-            <CreateTime>%s</CreateTime>
-            <MsgType><![CDATA[%s]]></MsgType>
-            <Music>
-            <Title><![CDATA[%s]]></Title>
-            <Description><![CDATA[%s]]></Description>
-            <MusicUrl><![CDATA[%s]]></MusicUrl>
-            <HQMusicUrl><![CDATA[%s]]></HQMusicUrl>
-            </Music>
-            </xml>
-EOF;
+            $musicTpl = '<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName> <CreateTime>%s</CreateTime><MsgType><![CDATA[%s]]></MsgType><Music><Title><![CDATA[%s]]></Title><Description><![CDATA[%s]]></Description><MusicUrl><![CDATA[%s]]></MusicUrl><HQMusicUrl><![CDATA[%s]]></HQMusicUrl></Music></xml>';
             if ($msgType == 'text') {
                 if ($keyword == "文本") {
                     //回复类型，如果是text，代表文本类型
